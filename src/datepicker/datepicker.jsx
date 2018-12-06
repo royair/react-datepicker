5dd50a9e2f3fa5e0870c9dfb518009c3c6c48223
Dmitry Shumsky <wumsky@gmail.com>
2018-12-06 10:43:37 +0200

    datepicker task

---
+++ src/datepicker/datepicker.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Datepicker extends Component {
+    render() {
+        return (
+            <div>datepicker here</div>
+        )
+    }
+}
+
+
+export default Datepicker;
