bfc8329b2228fa87940a920bc5cfbdb779786e79
roy <roy.yair@gmail.com>
2018-12-27 03:02:41 +0200

    finish basic layout and styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+
 import Datepicker from './datepicker/datepicker'
 
 class App extends Component {
