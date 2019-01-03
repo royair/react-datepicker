b99b97acea529fa3db3115925bc64ab84d15959e
roy yair <roy.yair@gmail.com>
2019-01-03 05:36:55 +0200

    Updates

---
+++ precache-manifest.0419bdfe42b99dbcfea9f66e1c8175ca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "38790c8e7a5ca95ad59e17ea3358f92c",
+    "url": "/react-datepicker/static/media/almoni-dl-aaa-700.38790c8e.eot"
+  },
+  {
+    "revision": "1447b6c5af4989b69516",
+    "url": "/react-datepicker/static/css/main.420c4932.chunk.css"
+  },
+  {
+    "revision": "63cc3e32bea33de64158",
+    "url": "/react-datepicker/static/js/1.63cc3e32.chunk.js"
+  },
+  {
+    "revision": "5ed7f5d19d0851ee4398",
+    "url": "/react-datepicker/static/js/runtime~main.5ed7f5d1.js"
+  },
+  {
+    "revision": "0462c0d35165d13eea7cd0d3c92e4ea3",
+    "url": "/react-datepicker/static/media/almoni-dl-aaa-400.0462c0d3.eot"
+  },
+  {
+    "revision": "1447b6c5af4989b69516",
+    "url": "/react-datepicker/static/js/main.1447b6c5.chunk.js"
+  },
+  {
+    "revision": "6ba69e75718e64dc1828a8a5447a664c",
+    "url": "/react-datepicker/static/media/almoni-dl-aaa-400.6ba69e75.woff"
+  },
+  {
+    "revision": "2a3fc485a7affec0b54fc18873f28f4c",
+    "url": "/react-datepicker/static/media/almoni-dl-aaa-700.2a3fc485.woff"
+  },
+  {
+    "revision": "ed117542092ee7c091f68d5fa863bc74",
+    "url": "/react-datepicker/static/media/almoni-dl-aaa-400.ed117542.ttf"
+  },
+  {
+    "revision": "823d79994cb94dc30681344c3c3eb4e3",
+    "url": "/react-datepicker/static/media/almoni-dl-aaa-700.823d7999.ttf"
+  },
+  {
+    "revision": "4cda23a6bbda2eb446bd4cfe4c0a4160",
+    "url": "/react-datepicker/index.html"
+  }
+];
