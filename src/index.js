09a04ff67a1fce4105bc554a453a6de0156bc598
roy yair <roy.yair@gmail.com>
2019-01-06 23:04:07 +0200

    reduce calendar store to one file. change root store to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'mobx-react';
-import Store from './store/store';
+import Store from './store';
 
 import './index.scss';
 
@@ -9,7 +9,12 @@ import App from './App';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Provider store={Store}><App/></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={Store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/store/calendar.js
+++ src/store/calendar.js
@@ -1,6 +1,5 @@
 import { decorate, observable, computed } from "mobx";
 import moment from "moment";
-import Month from './month';
 
 class Calendar {
   constructor() {
@@ -59,6 +58,83 @@ class Calendar {
   }
 }
 
+class Month {
+  constructor(startOfMonth) {
+    let monthNames       = ['ינואר', 'פברואר', 'מרץ', 'אפריל', 'מאי', 'יוני', 'יולי', 'אוגוסט', 'ספטמבר', 'אוקטובר', 'נובמבר', 'דצמבר'];
+    let today            = moment();
+    let endOfMonth       = moment(startOfMonth).endOf('month');
+    let endOfWeekMonth   = moment(startOfMonth).endOf('month').day('saturday');
+    let startOfWeekMonth = moment(startOfMonth).startOf('month').day('sunday');
+    let current          = moment(startOfWeekMonth);
+
+    this.year     = startOfMonth.year();
+    this.value    = startOfMonth.month();
+    this.id       = `${this.year}-${this.value}`;
+    this.name     = `${monthNames[this.value]} ${this.year}`;
+    this.selected = today.isSame(startOfMonth, 'month') ? true : false;
+    this.hovered  = false;
+    this.weeks    = [];
+
+    // run for each week of the month
+    while (current.isSameOrBefore(endOfWeekMonth, 'day')) {
+      let week = new Week(current, startOfMonth, endOfMonth);
+
+      this.weeks.push(week);
+
+      current.add(1, 'week');
+    }
+  }
+}
+
+class Week {
+  constructor(startOfWeek, startOfMonth, endOfMonth) {
+    this.id   = startOfWeek.week();
+    this.days = [];
+
+    // loop for 7 days of week
+    for (let i = 0; i < 7; i++) {
+      let date = new moment(startOfWeek).add(i, 'day');
+      let day  = date.isBetween(startOfMonth, endOfMonth, 'day', '[]')
+        ? new InRangeDay(date)
+        : new OutOfRangeDay(date);
+
+      this.days.push(day);
+    }
+    // change order of days to match rtl languages
+    this.days.replace(this.days.slice().reverse());
+  }
+}
+
+export class Day {
+  constructor(date) {
+    this.id       = date.format('x');
+    this.date     = moment(date);
+    this.prev     = undefined;
+    this.next     = undefined;
+    this.selected = false;
+  }
+}
+
+export class InRangeDay extends Day {
+  constructor(date) {
+    super(date);
+
+    let today = moment();
+
+    this.value      = date.format('D');
+    this.selectable = date.isSameOrAfter(today, 'day');
+  }
+}
+
+export class OutOfRangeDay extends Day {
+  constructor(date) {
+    super(date);
+
+    this.value      = undefined;
+    this.selectable = false;
+  }
+}
+
 decorate(Calendar,
   {
     months: observable,
@@ -66,6 +142,32 @@ decorate(Calendar,
     selectedMonth: computed,
     selectedDay: computed,
     hoveredMonth: computed,
+  });
+
+decorate(Month,
+  {
+    year: observable,
+    value: observable,
+    id: observable,
+    name: observable,
+    selected: observable,
+    hovered: observable,
+    weeks: observable,
+  });
+
+decorate(Week,
+  {
+    id: observable,
+    days: observable,
+  });
+
+decorate(Day,
+  {
+    id: observable,
+    date: observable,
+    prev: observable,
+    next: observable,
+    selected: observable,
   });
 
 export default Calendar;
